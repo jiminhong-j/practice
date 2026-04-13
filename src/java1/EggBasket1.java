cc0ba9fcc22e360027f9c34119760fdadc0db970
jiminhong-j <jiminh101@gmail.com>
2026-04-13 16:45:18 +0900

    j

---
+++ src/java1/EggBasket1.java
@@ -0,0 +1,14 @@
+package java1;
+
+public class EggBasket1 {
+    public static void main(String[] args){
+        int numberOfBaskets, eggsPerBasket, totalEggs;
+
+        long n = 0;
+        float x;
+        x = n;
+
+        eggsPerBasket = 6;
+        numberOfBaskets = 10;
+    }
+}
