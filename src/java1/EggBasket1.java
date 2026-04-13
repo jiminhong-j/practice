54ac6f6e6d9accc8f0cf98508914d9e5fba783eb
jiminhong-j <jiminh101@gmail.com>
2026-04-13 17:16:58 +0900

    casting

--- src/java1/EggBasket1.java
+++ src/java1/EggBasket1.java
@@ -2,13 +2,9 @@ package java1;
 
 public class EggBasket1 {
     public static void main(String[] args){
-        int numberOfBaskets, eggsPerBasket, totalEggs;
 
-        long n = 0;
-        float x;
-        x = n;
-
-        eggsPerBasket = 6;
-        numberOfBaskets = 10;
+        char answer = 'Y';
+        System.out.println(answer);
+        System.out.println((int)answer);
     }
 }
