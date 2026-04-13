54191e6fb248de41de9ef298159411f340bdf937
jiminhong-j <jiminh101@gmail.com>
2026-04-13 17:19:45 +0900

    truncation

--- src/java1/EggBasket1.java
+++ src/java1/EggBasket1.java
@@ -3,8 +3,15 @@ package java1;
 public class EggBasket1 {
     public static void main(String[] args){
 
-        char answer = 'Y';
-        System.out.println(answer);
-        System.out.println((int)answer);
+        int a = 4, b = 5, c;
+        double x = 1.5, y, z ;
+
+        y = b / x;
+        c = b / a;
+        z = b / a;
+
+        System.out.println("y = " + y);
+        System.out.println("c = " + c);
+        System.out.println("z = " + z);
     }
 }
