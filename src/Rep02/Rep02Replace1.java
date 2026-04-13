0941c3676e433033dfa362e00ea89761f8673783
jiminhong-j <jiminh101@gmail.com>
2026-04-13 21:29:20 +0900

    rep2-1

---
+++ src/Rep02/Rep02Replace1.java
@@ -0,0 +1,15 @@
+package Rep02;
+
+import java.util.Scanner;
+
+public class Rep02Replace1 {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter a line of text.");
+        String text = s.nextLine();
+
+        System.out.println("You entered: " + text);
+
+    }
+}
