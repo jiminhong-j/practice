5e00b2aa96f1b07e8e4aa0b20155c6f8026da4a3
jiminhong-j <jiminh101@gmail.com>
2026-04-18 18:12:54 +0900

    practice2

--- src/psMidterm/Main.java
+++ src/psMidterm/Main.java
@@ -1,19 +1,30 @@
 package psMidterm;
 
-import java.util.Scanner;
+import java.util.*;
 import psMidterm.model.Student;
 
 public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-
         // 여기에 작성
-        Student student = new Student();
-        System.out.print("이름 입력: ");
-        String name = s.nextLine();
-        System.out.print("점수 입력: ");
-        int score = s.nextInt();
-        student.setStudent(name, score);
-        student.printAll();
+        ArrayList<Student> list = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++){
+            Student student = new Student();
+            System.out.print("이름 입력: ");
+            String name = s.nextLine();
+            System.out.print("점수 입력: ");
+            int score = s.nextInt();
+            s.nextLine();
+            System.out.println();
+            student.setStudent(name, score);
+            list.add(student);
+        }
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.println(i+1 + "번 학생");
+            list.get(i).printAll();
+            System.out.println();
+        }
     }
 }
