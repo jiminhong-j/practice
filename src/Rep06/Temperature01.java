1f1673a4f0bdd7f5176e91182316bd2366afc31a
Jimin <jiminh101@gmail.com>
2026-04-25 21:10:01 +0900

    temperature01

---
+++ src/Rep06/Temperature01.java
@@ -0,0 +1,20 @@
+package Rep06;
+
+public class Temperature01 {
+    private double temperature;
+    private char unit;
+
+    public Temperature01(){
+        temperature = 0.0;
+        unit = 'C';
+    }
+
+    public Temperature01(double temperature){
+        this.temperature = temperature;
+        unit = 'C';
+    }
+
+    public void writeOutput(){
+        System.out.println("Temperature 01 = " + temperature + " degress " + unit + ".");
+    }
+}
